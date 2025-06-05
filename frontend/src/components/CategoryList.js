24158c01d1c955666967106ec8f0eee1d83d8df3
achrafelhadjaoui <aelhadjaoui23@gmail.com>
2025-06-05 20:47:20 +0100

    woking on category product

--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import SummaryApi from "../common";
+import { Link } from "react-router-dom";
 
 const CategoryList = () => {
   const [categoryProduct, setCategoryProduct] = useState([]);
@@ -27,16 +28,16 @@ const CategoryList = () => {
      <div className="flex items-center gap-4 justify-between overflow-scroll scrollbar-none">
      {categoryProduct.map((product, index) => {
         return (
-          <div className="cursor-pointer">
-            <div className="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-3 bg-white flex items-center justify-center">
+          <Link to={'/product-category/'+product?.category} className="cursor-pointer">
+            <div className="w-16 h-16 md:w-20 md:h-20 rounded-full overflow-hidden p-4 bg-slate-200 flex items-center justify-center">
               <img
                 src={product?.productImage[0]}
                 alt={product?.category}
-                className="h-full object-fill"
+                className="h-full object-scale-down mix-blend-multiply hover:scale-125 transition-all"
               />
             </div>
             <p className="text-center text-sm md:text-base capitalize">{product?.category}</p>
-          </div>
+          </Link>
         );
       })}
      </div>
